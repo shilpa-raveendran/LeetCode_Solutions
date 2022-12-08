6c8acff2ddbc1689b3b2af5cd8fbfec65be6cea1
Shilpa raveendran <113525124+shilpa-raveendran@users.noreply.github.com>
2022-12-08 08:51:55 -0500

    Q876 Middle of Linked List

---
+++ Middle of the Linked List_876.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode a = head;
+        ListNode b=head;
+
+        while(b!=null && b.next != null){
+            a=a.next;
+            b=b.next.next;
+        }
+        return a;
+    }
+}
