8cb95f089a827424a4a2afc8724e87086acc96a7
Shilpa raveendran <113525124+shilpa-raveendran@users.noreply.github.com>
2023-04-02 21:58:43 -0400

    Add files via upload

---
+++ Boats_to_Save_People_881.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0;
+        int j=people.length-1;
+        int count=0;
+        while(i<=j){
+            if(people[i]+people[j] > limit){
+                count++;
+                j--;
+            }else{
+                i++;
+                j--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
